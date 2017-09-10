dd6dabe9bdb37f077a9be622115259ec38c9b152
m1ch4lb3 <michal.brudnicki@gmail.com>
2017-09-10 14:05:28 +0200

    menu na enumie wersja dzialajaca + proozycja funkcji czyszczenia konsoli

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,9 @@
 public enum Menu {
+    EXIT(0, "Wyjście z programu"),
     FIND_GLOBAL_EXTREMES(1, "Wyszukaj ekstrema globalne"),
     FND_LOCALE_EXTREMES(2, "Wyszukaj ekstrema lokalne"),
     FIND_EXCHANGE_RATE(3, "Wczytaj kursy walut danych z plków"),
-    OPTIMIZE_DATA_FOR_IMPORT(4, "Zoptymalizuj dane w celu eksportu"),
-    EXIT(5, "Wyjście z programu");
+    OPTIMIZE_DATA_FOR_IMPORT(4, "Zoptymalizuj dane w celu eksportu");
 
     private int value;
     private String discription;
