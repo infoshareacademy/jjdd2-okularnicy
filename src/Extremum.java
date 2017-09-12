8bb53794be623515cca459774c386257ccb04b6f
Aleksandra Morawska <aleksandra_morawska@onet.pl>
2017-09-12 16:11:11 +0200

    extrema

--- src/Extremum.java
+++ src/Extremum.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 
 
 public class Extremum {
@@ -26,54 +24,17 @@ public class Extremum {
         this.min = min;
     }
 
-    public Fund min() {
-
-        Fund fund1 = new Fund("Fund1", LocalDate.of(2017, 9, 8), 100.00);
-        Fund fund2 = new Fund("Fund2", LocalDate.of(2017, 9, 8), 102.00);
-        Fund fund3 = new Fund("Fund3", LocalDate.of(2017, 9, 8), 104.00);
-        Fund fund4 = new Fund("Fund4", LocalDate.of(2017, 9, 8), 103.00);
-        Fund fund5 = new Fund("Fund5", LocalDate.of(2017, 9, 8), 101.00);
-
-        ArrayList<Fund> test = new ArrayList<Fund>();
-        test.add(fund1);
-        test.add(fund2);
-        test.add(fund3);
-        test.add(fund4);
-        test.add(fund5);
-
-        Collections.sort(test, new MyComparator(false));
-        setMin(test.get(0));
+    public Fund min(ArrayList<Fund> listToSortAsc) {
+        Collections.sort(listToSortAsc, new MyAscComparator());
+        setMin(listToSortAsc.get(0));
         return min;
-
     }
 
-    public Fund max() {
-
-        Fund fund1 = new Fund("Fund1", LocalDate.of(2017, 9, 8), 100.00);
-        Fund fund2 = new Fund("Fund2", LocalDate.of(2017, 9, 8), 102.00);
-        Fund fund3 = new Fund("Fund3", LocalDate.of(2017, 9, 8), 104.00);
-        Fund fund4 = new Fund("Fund4", LocalDate.of(2017, 9, 8), 103.00);
-        Fund fund5 = new Fund("Fund5", LocalDate.of(2017, 9, 8), 101.00);
-
-        ArrayList<Fund> test = new ArrayList<Fund>();
-        test.add(fund1);
-        test.add(fund2);
-        test.add(fund3);
-        test.add(fund4);
-        test.add(fund5);
-
-        Collections.sort(test, new MyComparator(true));
-        setMax(test.get(0));
+    public Fund max(ArrayList<Fund> listToSortDesc) {
+        Collections.sort(listToSortDesc, new MyDescComparator());
+        setMax(listToSortDesc.get(0));
         return max;
-
     }
-
-
-
-
-
-
-
 }
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,8 @@ import java.util.Collections;
 
 
 public class Main {
-
     public static void main(String[] args) {
 
-        Extremum extremum = new Extremum();
-
-        System.out.println(extremum.min());
-        System.out.println(extremum.max());
-
-
-
-    }
+   }
 }
 
