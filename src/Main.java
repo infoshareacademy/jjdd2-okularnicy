a00618ee3b1d52af7742a06ae46cacd198379b81
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-09-13 00:58:55 +0200

    read files names from command line

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("hello");
+        HashMap<String, String> filesHashMap = new HashMap<String, String>();//HashMap to store: Key(name of fund) Value(name of file)
 
+        StartingParameters startingParameters = new StartingParameters();//class to handle program arguments,
+        //reads specified files, gets names of funds and files, stores it in map
 
-        //pair programing Milena Daniel poczatek JD2O-10
-        //System.out.println("wypisywanie argumentow z jakimi jest uruchomiona aplikacja");
-
-        for (String pathToFile : args) {
-            //System.out.println(s);
-            FileReader fr = null;//FileReader to read from the file
-            BufferedReader br = null;//BufferedReader to buffer the values
-
-            try {
-                fr = new FileReader(pathToFile);//set the path to the FileReader
-                br = new BufferedReader(fr);//set the FileReader to BufferedReader
-                String sCurrentLine;//String variable that you use to take 1 line from the file
-                sCurrentLine = br.readLine();
-                sCurrentLine = br.readLine();
-                System.out.println(sCurrentLine);
-            } catch(FileNotFoundException ex) {
-                ex.printStackTrace();
-            } catch(IOException ex2){
-                ex2.printStackTrace();
-            }
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-        //System.out.println();
-        //System.out.println("pierwszy argument to:");
-        //System.out.println(args[0]);
-        //System.out.println();
-
-        //WczytajPlik wczytywaniePliku = new WczytajPlik();
-        //wczytywaniePliku.setSciezka(args[0]);
-        //System.out.println("sciezka to: " + wczytywaniePliku.sciezka);
-        //pair programing Milena Daniel koniec JD2O-10
+        startingParameters.startingParametersIntoMap(args, filesHashMap);//this method gets names of funds and files and stores it in map
+        startingParameters.printMap(filesHashMap);//this is only assistance method to print the content of entire map to check its correctness
     }
 }
---
+++ src/StartingParameters.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+
+public class StartingParameters {
+
+    public void printMap(Map<String, String> map){
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+        for(Map.Entry<String, String> entry : entries){
+            System.out.println("Key:[" + entry.getKey() + "] Value:[" + entry.getValue() + "]");
+        }
+    }
+
+    public void startingParametersIntoMap(String[] args, Map<String, String> map){
+
+        for (String pathToFile : args) {
+            FileReader fr = null;//FileReader to read from the file
+            BufferedReader br = null;//BufferedReader to buffer the values
+
+            try {
+                fr = new FileReader(pathToFile);//set the path to the FileReader
+                br = new BufferedReader(fr);//set the FileReader to BufferedReader
+                String sCurrentLine;//String variable that you use to take 1 line from the file
+                sCurrentLine = br.readLine();//firts 3 lines are ignored
+                sCurrentLine = br.readLine();
+                sCurrentLine = br.readLine();
+
+                while ((sCurrentLine = br.readLine()) != null){
+                    String separatorName1 = "(.txt)( )+";
+                    String name=sCurrentLine.split(separatorName1)[1].trim();
+
+                    String separatorFile1 = "(kB)( )+";
+                    String separatorFile2 = " ";
+                    String file = sCurrentLine.split(separatorFile1)[1].split(separatorFile2)[0];
+
+                    map.put(name,file);
+                }
+
+            } catch(FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch(IOException ex2){
+                ex2.printStackTrace();
+            }
+        }
+    }
+}
