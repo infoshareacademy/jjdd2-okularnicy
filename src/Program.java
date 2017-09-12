a5453688612d389c9932e1cb7e971aa011996126
m1ch4lb3 <michal.brudnicki@gmail.com>
2017-09-12 16:59:14 +0200

    początek zmian, klasa program

---
+++ src/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+
+    //tu musi powstać obiekt i mapa z plikami funduszy
+
+    //tu musi powstać obiekt i lista z notowaniami wybranego funduszu
+
+    //tu musi powstać obiekt i mapa z plikami walut
+
+    //tu musi powstać obiekt i lista z notowaniami wybranej waluty
+}
