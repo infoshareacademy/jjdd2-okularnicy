11907c9538d385811eb257c608ee3cfcb1ca21f7
Aleksandra Morawska <aleksandra_morawska@onet.pl>
2017-09-13 16:42:16 +0200

    ekstremum lokalne

--- src/LocalExtremum.java
+++ src/LocalExtremum.java
@@ -1,30 +1,42 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class LocalExtremum {
 
+    public Fund findMinByDate(List<Fund> operatingList, LocalDate startingDate, LocalDate endingDate) {
+
+        ArrayList<Fund> listInRange = new ArrayList<>();
+        Extremum extremum = new Extremum();
+
+        for (Fund iter : operatingList) {
+            if ((iter.getDate().isAfter(startingDate)) && (iter.getDate().isBefore(endingDate)) ||
+                    (iter.getDate().isEqual(startingDate)) || (iter.getDate().isEqual(endingDate))) {
+                listInRange.add(iter);
+            }
+        }
+        return extremum.min(listInRange);
+    }
+
+    public Fund findMaxByDate(List<Fund> operatingList, LocalDate startingDate, LocalDate endingDate) {
+
+        ArrayList<Fund> listInRange = new ArrayList<>();
+        Extremum extremum = new Extremum();
+
+        for (Fund iter : operatingList) {
+            if ((iter.getDate().isAfter(startingDate)) && (iter.getDate().isBefore(endingDate)) ||
+                    (iter.getDate().isEqual(startingDate)) || (iter.getDate().isEqual(endingDate))){
+                listInRange.add(iter);
+            }
+        }
+
+        return extremum.max(listInRange);
 
 
-    public ArrayList<Fund> findByDate(ArrayList<Fund> abcd, LocalDate startingDate, LocalDate endingDate){
-
-        Fund fund1 = new Fund("Fund1", LocalDate.of(2017, 9, 8), 100.00);
-        Fund fund2 = new Fund("Fund2", LocalDate.of(2017, 9, 8), 102.00);
-        Fund fund3 = new Fund("Fund3", LocalDate.of(2017, 9, 8), 104.00);
-        Fund fund4 = new Fund("Fund4", LocalDate.of(2017, 9, 8), 103.00);
-        Fund fund5 = new Fund("Fund5", LocalDate.of(2017, 9, 8), 101.00);
-
-        ArrayList<Fund> test = new ArrayList<Fund>();
-        test.add(fund1);
-        test.add(fund2);
-        test.add(fund3);
-        test.add(fund4);
-        test.add(fund5);
-
-        for (Fund iter : abcd){
 
 
-                    }
+    }
 
 }
 
---
+++ src/TestLocalExtremum.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class TestLocalExtremum {
+    public static void main(String[] args) {
+
+        Fund fund1 = new Fund("Fund1", LocalDate.of(2017, 9, 8), 100.00);
+        Fund fund2 = new Fund("Fund2", LocalDate.of(2017, 9, 8), 102.00);
+        Fund fund3 = new Fund("Fund3", LocalDate.of(2017, 9, 10), 104.00);
+        Fund fund4 = new Fund("Fund4", LocalDate.of(2017, 9, 11), 103.00);
+        Fund fund5 = new Fund("Fund5", LocalDate.of(2017, 9, 14), 101.00);
+
+        ArrayList<Fund> test = new ArrayList<Fund>();
+        test.add(fund1);
+        test.add(fund2);
+        test.add(fund3);
+        test.add(fund4);
+        test.add(fund5);
+
+        LocalDate startingDate = LocalDate.of(2017, 9, 7);
+        LocalDate endingDate = LocalDate.of(2017, 9, 11);
+
+        LocalExtremum localExtremum = new LocalExtremum();
+        localExtremum.findMinByDate(test, startingDate, endingDate);
+
+        System.out.println(localExtremum.findMinByDate(test, startingDate, endingDate).getClose());
+        System.out.println(localExtremum.findMaxByDate(test, startingDate, endingDate).getClose());
+    }
+}
