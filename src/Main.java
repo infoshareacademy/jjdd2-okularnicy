8b5ed9db664ff92cd1733258d44aec88b519762a
danski88 <danski88@users.noreply.github.com>
2017-09-14 16:47:55 +0200

    Feature/jd2 o 10 wczytanie ścieżki do plikow lst (#6)
    * subtask JD2O-10 done
    
    * zakomentowanie debugowania
    
    * read files names from command line - to be continued
    
    * shorten omegafun files
    
    * read files names from command line
    
    * poprawki do pull request

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<String, String> filesHashMap = new HashMap<String, String>();//HashMap to store: Key(name of fund) Value(name of file)
+
+        StartingParameters startingParameters = new StartingParameters();//class to handle program arguments,
+        //reads specified files, gets names of funds and files, stores it in map
+
+        filesHashMap.putAll(startingParameters.startingParametersIntoMap(args));//this method gets names of funds and files, stores it in map and return this map
+        startingParameters.printMap(filesHashMap);//this is only assistance method to print the content of entire map to check its correctness
+
         final String appName = "WITAJ W PROGRAMIE ANALIZATOR FINANSOWY";
         System.out.println(appName);
         System.out.println();
