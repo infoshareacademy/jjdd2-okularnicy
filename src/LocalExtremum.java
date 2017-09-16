b5095b651c3b810fb71f78268dad2c9680d5e3d7
aleksmoraw <aleksandra_morawska@onet.pl>
2017-09-16 12:44:24 +0200

    dodatnie wyjątków w wyszukiwaniu daty

--- src/LocalExtremum.java
+++ src/LocalExtremum.java
@@ -1,3 +1,4 @@
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -6,17 +7,52 @@ import java.util.Scanner;
 
 public class LocalExtremum {
 
-    Scanner scanner = new Scanner (System.in);
+    Scanner scanner = new Scanner(System.in);
+    private LocalDate startingDate;
+    private LocalDate endingDate;
+
+
+    public void setStartingDate(LocalDate startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public void setEndingDate(LocalDate endingDate) {
+        this.endingDate = endingDate;
+    }
+
+    public LocalDate getStartingDate() {
+        return startingDate;
+    }
+
+    public LocalDate getEndingDate() {
+        return endingDate;
+    }
 
     public Fund findMinByDate(List<Fund> operatingList) {
 
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println("Podaj pierwszą datę (yyyy-mm-dd)");
-        String textStartingDate = scanner.nextLine();
-        LocalDate startingDate = LocalDate.parse(textStartingDate, dateTimeFormatter);
-        System.out.println("Podaj drugą datę (yyyy-mm-dd)");
-        String textEndingDate = scanner.nextLine();
-        LocalDate endingDate = LocalDate.parse(textEndingDate, dateTimeFormatter);
+        System.out.println("Podaj przedział czasowy");
+
+        do {
+            System.out.println("Podaj pierwszą datę (yyyy-mm-dd)");
+            try {
+                setStartingDate(LocalDate.parse(scanner.nextLine()));
+            } catch (DateTimeException pe) {
+                System.out.println("Niepoprawny format daty. Spróbuj ponownie");
+            } catch (IndexOutOfBoundsException ie) {
+                if (startingDate.isBefore())
+            }
+        } while (getStartingDate() == null);
+
+        do {
+            System.out.println("Podaj drugą datę (yyyy-mm-dd)");
+            try {
+                setEndingDate(LocalDate.parse(scanner.nextLine()));
+            } catch (DateTimeException pe) {
+                System.out.println("Niepoprawny format daty. Spróbuj ponownie");
+            }
+        } while (getEndingDate() == null);
+        scanner.close();
 
         ArrayList<Fund> listInRange = new ArrayList<>();
         Extremum extremum = new Extremum();
@@ -26,7 +62,7 @@ public class LocalExtremum {
                 listInRange.add(iter);
             }
         }
-        return extremum.findMin(listInRange);
+                          return extremum.findMin(listInRange);
     }
 
     public Fund findMaxByDate(List<Fund> operatingList, LocalDate startingDate, LocalDate endingDate) {
@@ -34,7 +70,7 @@ public class LocalExtremum {
         Extremum extremum = new Extremum();
         for (Fund iter : operatingList) {
             if ((iter.getDate().isAfter(startingDate)) && (iter.getDate().isBefore(endingDate)) ||
-                    (iter.getDate().isEqual(startingDate)) || (iter.getDate().isEqual(endingDate))){
+                    (iter.getDate().isEqual(startingDate)) || (iter.getDate().isEqual(endingDate))) {
                 listInRange.add(iter);
             }
         }
