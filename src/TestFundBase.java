2b1a464ba4a53e14ebd1ec60ac5cd89a498b071f
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-09-17 17:35:56 +0200

    usuniecie zbednych linii

--- src/TestFundBase.java
+++ src/TestFundBase.java
@@ -13,17 +13,12 @@ public class TestFundBase {
         System.out.println();
         System.out.println("lista zawiera: " + myFundList.size() + " elementow");//printing the amount of rows that is inside the list
 
-        //printing the content from the list
+        //printing the content of the list
         System.out.println();
         System.out.println("wypisanie wszystkich elementow z wczytanej listy");
         for(Fund fundPrint: myFundList){
             System.out.print("getName:" + fundPrint.getName() + " ");
-            System.out.print("getDate:" + fundPrint.getDate() + " ");
-            //System.out.print("getOpen:" + fundPrint.getOpen() + " ");
-            //System.out.print("getHigh:" + fundPrint.getHigh() + " ");
-            //System.out.print("getLow:" + fundPrint.getLow() + " ");
             System.out.print("getClose:" + fundPrint.getClose() + " ");
-            //System.out.print("getVolume:" + fundPrint.getVolume() + " ");
             System.out.println();//'new line' is in separated command because amount of data obtained from the file probably will change
         }
     }
