b1d51a56c5132caa208da73f53a66a0d4c5b7461
Aleksandra Morawska <aleksandra_morawska@onet.pl>
2017-09-20 16:55:42 +0200

    mockowanie listy

---
+++ src/test/java/ExtremumTest.java
@@ -0,0 +1,22 @@
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ExtremumTest {
+
+    private static final Fund FUND1 = newFund("AGI001", 2);
+    private static final Fund FUND2 = newFund();
+
+    //AGI001,20090619,1000.00,1000.00,1000.00,1000.00,0
+
+
+
+
+
+
+}
+
+    @Mock
+    private ArrayList<Fund> fundlist;
+    private Extremum extremum;
