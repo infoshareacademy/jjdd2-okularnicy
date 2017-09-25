ab26a25c0fcf77d2b2db9797d801b8cf8ae20987
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-09-25 23:41:34 +0200

    unZip(zipFile, outputFolder) method to unzip the zip archive

--- baseapp/src/main/java/UnZip.java
+++ baseapp/src/main/java/UnZip.java
@@ -1,51 +1,41 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.List;
+package main.java;
+
+import java.io.*;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class UnZip {
 
-    private List<String> fileList;
-    private String pathToZip = "testdata/zip/omeganbp.zip";
-    private String pathToOutputFolder = "testdata/zip/unzip/";
-
-
-    public void unZipIt(String zipFile, String outputFolder){
+    public void unZip(String zipFile, String outputFolder){
 
         byte[] buffer = new byte[1024];
-
         File folder = new File(outputFolder);
         if(!folder.exists()){
             folder.mkdir();
         }
-
         ZipInputStream zipInputStream = null;
         try {
             zipInputStream = new ZipInputStream(new FileInputStream(zipFile));
             ZipEntry zipEntry = zipInputStream.getNextEntry();
-
             while(zipEntry!=null){
-
+                String fileName = zipEntry.getName();
+                File newFile = new File(outputFolder + File.separator + fileName);
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fileOutputStream = new FileOutputStream(newFile);
+                int length;
+                while((length = zipInputStream.read(buffer)) > 0){
+                    fileOutputStream.write(buffer, 0, length);
+                }
+                fileOutputStream.close();
+                zipEntry = zipInputStream.getNextEntry();
             }
+            zipInputStream.closeEntry();
+            zipInputStream.closeEntry();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException f) {
+            f.printStackTrace();
         }
-
-
-
-
-
-
-
     }
-
-
-
-
 }
