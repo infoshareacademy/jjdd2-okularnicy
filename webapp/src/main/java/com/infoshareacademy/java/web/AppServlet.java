b4bbe079ffb4d9e5757d3c9d7042accbfca78e60
milena <mtrutkowska@gmail.com>
2017-09-25 12:40:59 +0200

    wyswietlanie pierwszej strony

--- webapp/src/main/java/com/infoshareacademy/java/web/AppServlet.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/AppServlet.java
@@ -1,20 +1,20 @@
 package com.infoshareacademy.java.web;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.rmi.ServerException;
 
 @WebServlet("/analizator")
-public class AppServlet extends HttpServlet{
+public class AppServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException {
 
-        resp.setContentType("text/html;charset=UTF-8");
+        resp.setContentType("text/html:charset=UTF-8");
         PrintWriter writer = resp.getWriter();
 
 
