183a2ed22138e7156baeca7873b55b97031c5d3f
aleksmoraw <31630697+aleksmoraw@users.noreply.github.com>
2017-09-26 16:28:19 +0200

    Testy/refactor (#29)
    * testy
    
    * refactoring - testy

--- baseapp/src/test/java/ExtremumTest.java
+++ baseapp/src/test/java/ExtremumTest.java
@@ -8,17 +8,9 @@ import static org.junit.Assert.assertThat;
 
 public class ExtremumTest {
 
-    private static final Fund FUND1 = newFund("SEB001", LocalDate.of(1998, 6,1), 100.00);
-    private static final Fund FUND2= newFund("SEB001", LocalDate.of(2011, 1,7), 212.27);
-    private static final Fund FUND3= newFund("SEB001", LocalDate.of(2015, 8,13), 179.02);
-
-    private static Fund newFund(String name, LocalDate date, Double close) {
-        Fund f = new Fund();
-        f.setName(name);
-        f.setDate(date);
-        f.setClose(close);
-        return f;
-    }
+    private static final Fund FUND1 = new Fund("SEB001", LocalDate.of(1998, 6,1), 100.00);
+    private static final Fund FUND2= new Fund("SEB001", LocalDate.of(2011, 1,7), 212.27);
+    private static final Fund FUND3= new Fund("SEB001", LocalDate.of(2015, 8,13), 179.02);
 
     @Test
     public void isFindMaxAndFindMinIsCorrect () {
