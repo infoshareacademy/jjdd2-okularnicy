bf2ef9c8ed344d7c57d5a44a757546723ba217bb
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-09-27 17:50:47 +0200

    removing unnecessary files

--- webapp/src/main/java/Servlet.java
+++ webapp/src/main/java/Servlet.java
@@ -1,4 +1,3 @@
-/*
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -6,9 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 @WebServlet("/index")
@@ -22,24 +19,52 @@ public class Servlet extends HttpServlet {
         writer.println("<!DOCTYPE html>");
         writer.println("<html>");
         writer.println("<body>");
-
         writer.println("<form action=\"index\" method=\"post\" enctype=\"multipart/form-data\">");
-        writer.println("<input type=\"file\" name=\"userfile\"/>");
+        writer.println("<p>Tutaj wskaż plik LST: ");
+        writer.println("<input type=\"file\" name=\"fileLST\"/>");
+        writer.println("</p>");
+        writer.println("<p>Tutaj wskaż archiwum ZIP: ");
+        writer.println("<input type=\"file\" name=\"fileZIP\"/>");
+        writer.println("</p>");
         writer.println("<button type=\"submit\">Wyslij</button>");
         writer.println("</form>");
-
         writer.println("</body>");
         writer.println("</html>");
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Part userfile = req.getPart("userfile");
-        InputStream inputStream = userfile.getInputStream();
-        Scanner scanner = new Scanner(inputStream);
-        while(scanner.hasNextLine()){
-            resp.getWriter().println(scanner.nextLine());
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            Part fileLST = null;
+            Part fileZIP = null;
+            fileLST = req.getPart("fileLST");
+            fileZIP = req.getPart("fileZIP");
+            InputStream inputStreamLST = null;
+            InputStream inputStreamZIP = null;
+            inputStreamLST = fileLST.getInputStream();
+            inputStreamZIP = fileZIP.getInputStream();
+            String tmpDir = System.getProperty("java.io.tmpdir");
+            String LSTDir = tmpDir + "/plik.lst.txt";
+            String ZIPDir = tmpDir + "/plik.zip.txt";
+            OutputStream outputStreamLST = null;
+            OutputStream outputStreamZIP = null;
+            outputStreamLST = new FileOutputStream(new File(LSTDir));
+            outputStreamZIP = new FileOutputStream(new File(ZIPDir));
+            int readLST = 0;
+            int readZIP = 0;
+            byte[] bytesLST = new byte[1024];
+            byte[] bytesZIP = new byte[1024];
+            while ((readLST = inputStreamLST.read(bytesLST)) != -1) {
+                outputStreamLST.write(bytesLST, 0, readLST);
+            }
+            while ((readZIP = inputStreamZIP.read(bytesZIP)) != -1) {
+                outputStreamZIP.write(bytesZIP, 0, readZIP);
+            }
+            resp.getWriter().println("done");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
         }
     }
 }
-*/
