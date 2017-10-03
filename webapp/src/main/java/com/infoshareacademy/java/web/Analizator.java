b47a968b41c7333849f8c796bc0dbaed27d261d7
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-10-04 01:32:03 +0200

    loading files map and printing it in the browser using servlet

--- webapp/src/main/java/com/infoshareacademy/java/web/Analizator.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/Analizator.java
@@ -1,5 +1,7 @@
 package com.infoshareacademy.java.web;
 
+import com.infoshareacademy.baseapp.StartingParameters;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +12,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.rmi.ServerException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
 @WebServlet("/analizator")
 public class Analizator extends HttpServlet {
@@ -17,12 +22,41 @@ public class Analizator extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServerException, IOException, ServletException {
 
-        RequestDispatcher dispatcher = getServletContext()
-                .getRequestDispatcher ("/WEB-INF/analizator.jsp");
-        req.setAttribute("date", LocalDateTime.now());
-        dispatcher.forward(req, resp);
 
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter writer = resp.getWriter();
+        writer.println("<p>cos</p>");
+
+        HashMap<String, String> filesHashMap = new HashMap<String, String>();
+        StartingParameters startingParameters = new StartingParameters();
+        writer.println("</p>");
+        writer.println(getServletContext().getAttribute("LSTDir").toString());
+        writer.println("</p>");
+
+        String path = getServletContext().getAttribute("LSTDir").toString();
+        String[] pathArray = new String[] {path};
+        filesHashMap.putAll(startingParameters.startingParametersIntoMap(pathArray));
+
+        int i = 1;
+        Set<Entry<String, String>> entries = filesHashMap.entrySet();
+        for(Entry<String, String> entry : entries){
+            writer.println("<p>");
+            writer.println("iteracja nr ");
+            writer.println(i);
+            writer.println(" key:");
+            writer.println( entry.getKey());
+            writer.println(" value:");
+            writer.println(entry.getValue());
+            writer.println("</p>");
+
+        }
+
+
+
+
+        /*RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher ("/WEB-INF/analizator.jsp");
+        req.setAttribute("date", LocalDateTime.now());
+        dispatcher.forward(req, resp);*/
     }
 }
