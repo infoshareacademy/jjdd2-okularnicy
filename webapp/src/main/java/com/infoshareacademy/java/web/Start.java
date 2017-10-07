aa5c89be4642ac1ae72dea5b165a002c549fd463
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-10-07 17:19:54 +0200

    add loggers from m1ch4lb3

--- webapp/src/main/java/com/infoshareacademy/java/web/Start.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/Start.java
@@ -1,5 +1,9 @@
 package com.infoshareacademy.java.web;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -13,6 +17,8 @@ import java.io.*;
 @WebServlet("/start")
 @MultipartConfig
 public class Start extends HttpServlet{
+
+    private final Logger logger = LogManager.getLogger("log4j-burst-filter");
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -61,9 +67,9 @@ public class Start extends HttpServlet{
 
             resp.sendRedirect("start2");
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.log(Level.ERROR, "Wyjątek: IOException");
         } catch (ServletException e) {
-            e.printStackTrace();
+            logger.log(Level.ERROR, "Wyjątek: ServletException");
         }
     }
 }
