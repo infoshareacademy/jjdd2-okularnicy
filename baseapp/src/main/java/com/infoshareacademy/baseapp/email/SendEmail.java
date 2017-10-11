cc42c313205ca6468833428e7213f15027096a2c
Daniel Skwierawski <skwierawski.daniel@gmail.com>
2017-10-11 17:00:16 +0200

    start with SendEmail class

---
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/email/SendEmail.java
@@ -0,0 +1,15 @@
+package com.infoshareacademy.baseapp.email;
+
+import java.util.Properties;
+
+public class SendEmail {
+
+    public static void main(String[] args) {
+        String to = "skwierawski.daniel@gmail.com";
+        String from = "skwierawski.daniel@gmail.com";
+        String host = "localhost";
+        Properties properties = System.getProperties();
+
+    }
+
+}
