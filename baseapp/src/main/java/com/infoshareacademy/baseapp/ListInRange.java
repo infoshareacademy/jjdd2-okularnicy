ab8026251ef9071096111e16bfff48bc6aa66e2c
aleksmoraw <31630697+aleksmoraw@users.noreply.github.com>
2017-10-11 16:21:11 +0200

    Feature/jd2 o 70 (#43)
    * usuniecie klas niewykorzystywanych w aplikacji webowej
    
    * test klasy FundBase
    
    * test klasy startingParameters

--- baseapp/src/main/java/com/infoshareacademy/baseapp/ListInRange.java
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/ListInRange.java
@@ -11,29 +11,8 @@ public class ListInRange {
 
     private final Program program;
 
-    private final Logger logger = LogManager.getLogger("log4j-burst-filter");
-
     public ListInRange(Program program) {
         this.program = program;
-    }
-
-    public void isDateIsInRange() {
-        LocalDate startDate = program.getStartDate();
-        LocalDate endDate = program.getEndDate();
-        Fund firstFund = program.getFundsList().get(0);
-        Fund lastFund = program.getFundsList().get(program.getFundsList().size()-1);
-
-        if (startDate.isBefore(firstFund.getDate()) || endDate.isAfter(lastFund.getDate())) {
-            System.out.println("Podany zakres wykracza poza dostępne dane.");
-            logger.log(Level.WARN, "Podany zakres wykracza poza dostępne dane." + "(" + startDate + " - " + endDate + ")");
-        } else if (startDate.isBefore(firstFund.getDate()) && endDate.isBefore(firstFund.getDate()) ||
-                startDate.isAfter(lastFund.getDate()) && endDate.isBefore(lastFund.getDate())) {
-            System.out.println("Brak danych dla podanego przedziału czasu. Spróbuj ponownie.");
-            logger.log(Level.WARN, "Brak danych dla podanego przedziału czasu. Spróbuj ponownie." + "(" + startDate + " - " + endDate + ")");
-        } else {
-            System.out.println("Wczytano poprawnie dane z podanego zakresu.");
-            logger.log(Level.INFO, "Wczytano poprawnie dane z podanego zakresu.");
-        }
     }
 
     public List<Fund> setListInRange () {
