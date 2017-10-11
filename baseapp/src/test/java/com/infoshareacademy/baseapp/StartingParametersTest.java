f1cde62d493a5dd21e251473ed3e5c8ab243ff81
Aleksandra Morawska <aleksandra_morawska@onet.pl>
2017-10-11 16:15:10 +0200

    test klasy startingParameters

---
+++ baseapp/src/test/java/com/infoshareacademy/baseapp/StartingParametersTest.java
@@ -0,0 +1,32 @@
+package com.infoshareacademy.baseapp;
+
+import org.junit.Test;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StartingParametersTest {
+
+    private final static String FILENAME = "/omegafuntest.lst";
+
+    @Test
+    public void isReadFoundIntoListIsCorrect() {
+
+        URL url = this.getClass().getResource(FILENAME);
+        String absolutePath = url.getPath();
+        String[] tablica = {absolutePath};
+
+        StartingParameters startingParameters = new StartingParameters();
+        Map<String, String> resultMap = startingParameters.startingParametersIntoMap(tablica);
+
+        assertThat(resultMap.size(), is(4));
+        assertTrue(resultMap.containsKey("NOVO Akcji"));
+
+    }
+}
+
