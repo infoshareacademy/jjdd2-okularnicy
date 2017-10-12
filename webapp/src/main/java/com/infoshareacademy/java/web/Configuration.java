803b8475efc4f8309ae1bcd1103ff846bbdf3039
michalbrudnicki <michal.brudnicki@gmail.com>
2017-10-12 17:01:14 +0200

    Określenie zmiennych, które mają znaleźć się w zewnętnym pliku

---
+++ webapp/src/main/java/com/infoshareacademy/java/web/Configuration.java
@@ -0,0 +1,29 @@
+package com.infoshareacademy.java.web;
+
+public class Configuration {
+    private String unzippeDir;
+    private String lstDir;
+    private String zipDir;
+    private String workFiles;
+    private String targetDir;
+
+    public String getUnzippeDir() {
+        return unzippeDir;
+    }
+
+    public String getLstDir() {
+        return lstDir;
+    }
+
+    public String getZipDir() {
+        return zipDir;
+    }
+
+    public String getWorkFiles() {
+        return workFiles;
+    }
+
+    public String getTargetDir() {
+        return targetDir;
+    }
+}
