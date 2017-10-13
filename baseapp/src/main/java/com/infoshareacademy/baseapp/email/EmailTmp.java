75d62868ecc602ff3b8a1648b91444c10970a79d
Daniel Skwierawski <skwierawski.daniel@gmail.com>
2017-10-13 16:46:11 +0200

    sample public static void main class

---
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/email/EmailTmp.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.baseapp.email;
+
+public class EmailTmp {
+
+    public static void main(String[] args) {
+        Email email = new Email("infoshareokularnicy@wp.pl", "okularnicY", "smtp.wp.pl", 465);
+        email.send("infoshareokularnicy@wp.pl", "subject", "hello world");
+    }
+
+
+}
