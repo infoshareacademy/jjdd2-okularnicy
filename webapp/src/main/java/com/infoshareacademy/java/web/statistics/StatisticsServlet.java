b21870f02acfddcdd6b8ebf36177859d519ee9a7
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-10-29 19:04:39 +0100

    initial work

--- webapp/src/main/java/com/infoshareacademy/java/web/statistics/StatisticsServlet.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/statistics/StatisticsServlet.java
@@ -1,10 +1,8 @@
 package com.infoshareacademy.java.web.statistics;
 
-import com.infoshareacademy.baseapp.statistics.Record;
 import com.infoshareacademy.baseapp.statistics.Statistics;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,77 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @WebServlet("/finanse/statistics")
-public class StatisticsServlet extends HttpServlet{
-    
+public class StatisticsServlet extends HttpServlet {
+
     private Statistics statistics = Statistics.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append("jestes w statystykach" + "\n");
-        sb.append("" + "\n");
-        sb.append("" + "\n");
-
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime tenSecondsAgo = now.minusSeconds(10);
-        LocalDateTime twentySecondsAgo = now.minusSeconds(20);
-
-        sb.append("Calkowita liczba wyborow: " + statistics.getNumberOfVisits() + "\n");
-        List<Record> recordsList = statistics.getAll();
-        Map<String, Integer> numberOfVisitsEachName = new HashMap<>();
-        for(Record record: recordsList){
-            if ( numberOfVisitsEachName.containsKey(record.getName()) ){
-                numberOfVisitsEachName.put(record.getName(), numberOfVisitsEachName.get(record.getName())+1);
-            } else {
-                numberOfVisitsEachName.put(record.getName(),1);
-            }
-        }
-        sb.append(numberOfVisitsEachName.toString() + "\n");
-        sb.append("" + "\n");
-        sb.append("" + "\n");
-
-        sb.append("w ciagu ostatnich 10 sekund: " + statistics.getNumberOfVisits(tenSecondsAgo, now) + "\n");
-        recordsList = statistics.getAll(tenSecondsAgo, now);
-        numberOfVisitsEachName = new HashMap<>();
-        for(Record record: recordsList){
-            if ( numberOfVisitsEachName.containsKey(record.getName()) ){
-                numberOfVisitsEachName.put(record.getName(), numberOfVisitsEachName.get(record.getName())+1);
-            } else {
-                numberOfVisitsEachName.put(record.getName(),1);
-            }
-        }
-        sb.append(numberOfVisitsEachName.toString() + "\n");
-        sb.append("" + "\n");
-        sb.append("" + "\n");
-
-        sb.append("w ciagu ostatnich 20 sekund: " + statistics.getNumberOfVisits(twentySecondsAgo, now) + "\n");
-        recordsList = statistics.getAll(twentySecondsAgo, now);
-        numberOfVisitsEachName = new HashMap<>();
-        for(Record record: recordsList){
-            if ( numberOfVisitsEachName.containsKey(record.getName()) ){
-                numberOfVisitsEachName.put(record.getName(), numberOfVisitsEachName.get(record.getName())+1);
-            } else {
-                numberOfVisitsEachName.put(record.getName(),1);
-            }
-        }
-        sb.append(numberOfVisitsEachName.toString() + "\n");
-        sb.append("" + "\n");
-        sb.append("" + "\n");
-
-        sb.append("Historia" + "\n");
-        recordsList = statistics.getAll();
-        for(Record record: recordsList){
-            sb.append(record.getName() + ">>" + record.getDateTime() + "\n");
-        }
-        sb.append("" + "\n");
-        sb.append("" + "\n");
-        resp.getWriter().println(sb.toString());
+        LocalDateTime last1 = now.minusSeconds(10);
+        LocalDateTime last2 = now.minusSeconds(20);
+        getServletContext().setAttribute("NumberOfVisitsLast1", statistics.getNumberOfVisits(last1, now));
+        getServletContext().setAttribute("OccurrenceMapLast1", statistics.getOccurrenceMap(last1, now));
+        getServletContext().setAttribute("NumberOfVisitsLast2", statistics.getNumberOfVisits(last2, now));
+        getServletContext().setAttribute("OccurrenceMapLast2", statistics.getOccurrenceMap(last2, now));
+        getServletContext().setAttribute("NumberOfVisitsTotal", statistics.getNumberOfVisits());
+        getServletContext().setAttribute("OccurrenceMapTotal", statistics.getOccurrenceMap());
+        getServletContext().setAttribute("RecordsListTotal", statistics.getAll());
+        RequestDispatcher dispatcher = getServletContext()
+                .getRequestDispatcher("/WEB-INF/statisticsDoGet.jsp");
+        dispatcher.forward(req, resp);
     }
 }
---
+++ webapp/src/main/webapp/WEB-INF/statisticsDoGet.jsp
@@ -0,0 +1,94 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <jsp:include page="/shared/head.jsp"/>
+</head>
+
+<body>
+<header>
+    <jsp:include page="/shared/header.jsp"/>
+</header>
+<div class="statistics" id="stat">
+    <table class="center">
+        <thead>
+        <tr>
+            <th colspan="2"><H1>STATYSTYKI</H1></th>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+            <td><H3>Liczba wyborów użytkownika w pierwszym przedziale czasu</H3></td>
+            <td><H3>${NumberOfVisitsLast1}</H3></td>
+        </tr>
+        <tr>
+            <td>
+                <table class="center adjusted">
+                    <tbody>
+                    <c:forEach items="${OccurrenceMapLast1}" var="entry">
+                    <tr>
+                        <td>${entry.key}</td>
+                        <td>${entry.value}</td>
+                    </tr>
+                    </c:forEach>
+                    </tbody>
+                </table>
+        </tr>
+        <tr>
+            <td><H3>Liczba wyborów użytkownika w drugim przedziale czasu</H3></td>
+            <td><H3>${NumberOfVisitsLast2}</H3></td>
+        </tr>
+        <tr>
+            <td>
+                <table class="center adjusted">
+                    <tbody>
+                    <c:forEach items="${OccurrenceMapLast2}" var="entry">
+                        <tr>
+                            <td>${entry.key}</td>
+                            <td>${entry.value}</td>
+                        </tr>
+                    </c:forEach>
+                    </tbody>
+                </table>
+        </tr>
+        <tr>
+            <td><H3>Liczba wszystkich wyborów użytkownika</H3></td>
+            <td><H3>${NumberOfVisitsTotal}</H3></td>
+        </tr>
+        <tr>
+            <td>
+                <table class="center adjusted">
+                    <tbody>
+                    <c:forEach items="${OccurrenceMapTotal}" var="entry">
+                        <tr>
+                            <td>${entry.key}</td>
+                            <td>${entry.value}</td>
+                        </tr>
+                    </c:forEach>
+                    </tbody>
+                </table>
+        </tr>
+        </tbody>
+    </table>
+
+    <table class="center">
+        <thead>
+        <tr>
+            <th colspan="2"><H1>HISTORIA</H1></th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${RecordsListTotal}" var="entry">
+            <tr>
+                <td>${entry.name}</td>
+                <td>${entry.dateTime}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
