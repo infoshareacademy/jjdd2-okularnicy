19cf7489374964e9b0fb394492d8f11c53f3cd1b
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-05 22:54:54 +0100

    email has been sent

--- webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.java.web.timer;
 
+import com.infoshareacademy.baseapp.email.EmailService;
 import com.infoshareacademy.java.web.beans.UserFactory;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -12,6 +13,7 @@ import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import javax.mail.MessagingException;
 
 @Startup
 @Singleton
@@ -30,9 +32,18 @@ public class Timer {
 
     @Schedule(second="*/1", minute="*",hour="*", persistent=false)
     public void doWork(){
+        EmailService email = new EmailService("infoshareokularnicy@wp.pl", "okularnicY", "smtp.wp.pl", 465);
         logger.log(Level.INFO, "udalo sie!");
         boolean response = timerInfo.getInfo();
         logger.log(Level.INFO, "response=" + response);
+        if (response) {
+            try {
+                email.send("infoshareokularnicy@wp.pl", "subject", "hello world");
+                logger.log(Level.INFO, "email has been sent");
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 
