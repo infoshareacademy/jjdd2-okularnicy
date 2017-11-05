0024c93c5076d19f486e0c42e6239ee8b7f8a72b
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-06 00:43:22 +0100

    extract report method

--- webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
@@ -1,13 +1,12 @@
 package com.infoshareacademy.java.web.timer;
 
 import com.infoshareacademy.baseapp.email.EmailService;
+import com.infoshareacademy.baseapp.statistics.Report;
 import com.infoshareacademy.baseapp.statistics.Statistics;
 import com.infoshareacademy.java.web.beans.UserFactory;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
@@ -32,81 +31,27 @@ public class Timer {
     TimerConfiguration timerConfiguration = new TimerConfiguration();
     TimerJsonReader timerJsonReader = new TimerJsonReader();
     private Statistics statistics = Statistics.getInstance();
+    String json;
 
     @Inject
     TimerInfo timerInfo;
 
     @PostConstruct
     void initializeTimer  () {
-        logger.log(Level.INFO, "obiekt Timer utworzony");
-
-    }
-
-    @Schedule(second="*/1", minute="*",hour="*", persistent=false)
-    public void doWork(){
+        logger.log(Level.INFO, "obiekt Timer zostal utworzony");
         ResourceReader resourceReader = new ResourceReader();
-        String json = resourceReader.getStringFromResource("TimerConfig.json");
-
+        json = resourceReader.getStringFromResource("TimerConfig.json");
         try {
             timerConfiguration = timerJsonReader.readJsonFile(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        Duration duration1;
-        Duration duration2;
-        duration1 = Duration.ofDays(7)
-                .plusHours(0)
-                .plusMinutes(0)
-                .plusSeconds(0);
-        duration2 = Duration.ofDays(30)
-                .plusHours(0)
-                .plusMinutes(0)
-                .plusSeconds(0);
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime last1 = now.minus(duration1);
-        LocalDateTime last2 = now.minus(duration2);
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("<H1>STATYSTYKI</H1>");
-
-        sb.append("<table class=\"center\">");
-        sb.append("<tr class=\"tr1st\">");
-        sb.append("<td class=\"left\"><H3>Liczba wyborów użytkownika w pierwszym przedziale czasu</H3>");
-        sb.append("<p>(dni:7, godziny:0, minuty:0, sekundy:0)</p></td>");
-        sb.append("<td><H3>" + statistics.getNumberOfVisits(last1, now) + "</H3></td>");
-        sb.append("</tr>");
-        Map<String, Integer> numberOfVisitsEachName = statistics.getOccurrenceMap(last1, now);
-        Set<Entry<String, Integer>> entries = numberOfVisitsEachName.entrySet();
-        for(Entry<String, Integer> entry : entries){
-            sb.append("<tr>");
-            sb.append("<td class=\"left\">" + entry.getKey() + "</td>");
-            sb.append("<td>" + entry.getValue() + "</td>");
-            sb.append("</tr>");
-        }
-        sb.append("</table>");
-        sb.append("<br/>");
-
-        sb.append("<table class=\"center\">");
-        sb.append("<tr class=\"tr1st\">");
-        sb.append("<td class=\"left\"><H3>Liczba wyborów użytkownika w drugim przedziale czasu</H3>");
-        sb.append("<p>(dni:30, godziny:0, minuty:0, sekundy:0)</p></td>");
-        sb.append("<td><H3>" + statistics.getNumberOfVisits(last2, now) + "</H3></td>");
-        sb.append("</tr>");
-        numberOfVisitsEachName = statistics.getOccurrenceMap(last2, now);
-        entries = numberOfVisitsEachName.entrySet();
-        for(Entry<String, Integer> entry : entries){
-            sb.append("<tr>");
-            sb.append("<td class=\"left\">" + entry.getKey() + "</td>");
-            sb.append("<td>" + entry.getValue() + "</td>");
-            sb.append("</tr>");
-        }
-        sb.append("</table>");
-        sb.append("<br/>");
-
-        String message = sb.toString();
+    @Schedule(second="*", minute="*/1",hour="*", persistent=false)
+    public void doWork(){
+        Report report = new Report();
+        String message = report.getReport();
 
 
 
