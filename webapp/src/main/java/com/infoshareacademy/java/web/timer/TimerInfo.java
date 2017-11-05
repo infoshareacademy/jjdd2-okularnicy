3443e7fbed3368d1f6b876bc49fae8cb575bd12b
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-05 21:57:44 +0100

    InputStream from json file works

--- webapp/src/main/java/com/infoshareacademy/java/web/timer/TimerInfo.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/timer/TimerInfo.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
+import java.io.InputStream;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
@@ -23,8 +24,27 @@ public class TimerInfo {
         lastRun = LocalDateTime.now();
         logger.log(Level.INFO, "lastRun=" + lastRun);
         setLengthOfTime = Duration.ofSeconds(3);
-
         logger.log(Level.INFO, "setLengthOfTime=" + setLengthOfTime);
+
+        String json = "";
+        InputStream is = getClass().getClassLoader().getResourceAsStream("TimerConfig.json");
+        logger.log(Level.INFO, "ABCDInputStream=" + is);
+        try {
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        logger.log(Level.INFO, "JSON=" + json);
+
+
+
+
+
+
     }
 
     public boolean getInfo(){
