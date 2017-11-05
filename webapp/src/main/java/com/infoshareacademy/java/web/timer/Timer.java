b594fe1bef8af7810965ebc01541ff37fe4e2d54
Daniel.Skwierawski <skwierawski.daniel@gmail.com>
2017-11-05 23:53:45 +0100

    simple report in scheduled email

--- webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
+++ webapp/src/main/java/com/infoshareacademy/java/web/timer/Timer.java
@@ -19,6 +19,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 @Startup
 @Singleton
@@ -75,13 +79,43 @@ public class Timer {
         LocalDateTime last2 = now.minus(duration2);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("NumberOfVisitsLast1=" + statistics.getNumberOfVisits(last1, now));
-        sb.append("OccurrenceMapLast1=" + statistics.getOccurrenceMap(last1, now));
-        sb.append("NumberOfVisitsLast2=" + statistics.getNumberOfVisits(last2, now));
-        sb.append("OccurrenceMapLast2=" + statistics.getOccurrenceMap(last2, now));
-        sb.append("NumberOfVisitsTotal=" + statistics.getNumberOfVisits());
-        sb.append("OccurrenceMapTotal=" + statistics.getOccurrenceMap());
-        sb.append("RecordsListTotal=" + statistics.getAll());
+
+        sb.append("<H1>STATYSTYKI</H1>");
+
+        sb.append("<table class=\"center\">");
+        sb.append("<tr class=\"tr1st\">");
+        sb.append("<td class=\"left\"><H3>Liczba wyborów użytkownika w pierwszym przedziale czasu</H3>");
+        sb.append("<p>(dni:7, godziny:0, minuty:0, sekundy:0)</p></td>");
+        sb.append("<td><H3>" + statistics.getNumberOfVisits(last1, now) + "</H3></td>");
+        sb.append("</tr>");
+        Map<String, Integer> numberOfVisitsEachName = statistics.getOccurrenceMap(last1, now);
+        Set<Entry<String, Integer>> entries = numberOfVisitsEachName.entrySet();
+        for(Entry<String, Integer> entry : entries){
+            sb.append("<tr>");
+            sb.append("<td class=\"left\">" + entry.getKey() + "</td>");
+            sb.append("<td>" + entry.getValue() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        sb.append("<br/>");
+
+        sb.append("<table class=\"center\">");
+        sb.append("<tr class=\"tr1st\">");
+        sb.append("<td class=\"left\"><H3>Liczba wyborów użytkownika w drugim przedziale czasu</H3>");
+        sb.append("<p>(dni:30, godziny:0, minuty:0, sekundy:0)</p></td>");
+        sb.append("<td><H3>" + statistics.getNumberOfVisits(last2, now) + "</H3></td>");
+        sb.append("</tr>");
+        numberOfVisitsEachName = statistics.getOccurrenceMap(last2, now);
+        entries = numberOfVisitsEachName.entrySet();
+        for(Entry<String, Integer> entry : entries){
+            sb.append("<tr>");
+            sb.append("<td class=\"left\">" + entry.getKey() + "</td>");
+            sb.append("<td>" + entry.getValue() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        sb.append("<br/>");
+
         String message = sb.toString();
 
 
