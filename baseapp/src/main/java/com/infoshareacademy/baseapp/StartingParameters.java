2fe3dd0d1674c5d8fcdcbb15fa7e6fbf49a0f74b
michalbrudnicki <michal.brudnicki@gmail.com>
2017-11-05 11:25:07 +0100

    another push works fine

--- baseapp/src/main/java/com/infoshareacademy/baseapp/StartingParameters.java
+++ baseapp/src/main/java/com/infoshareacademy/baseapp/StartingParameters.java
@@ -57,10 +57,8 @@ public class StartingParameters {
                 }
 
             } catch (FileNotFoundException ex) {
-                ex.printStackTrace();
                 logger.log(Level.ERROR, "Wyjątek: FileNotFoundException");
             } catch (IOException ex2) {
-                ex2.printStackTrace();
                 logger.log(Level.ERROR, "Wyjątek: FileNotFoundException");
             }
         }
